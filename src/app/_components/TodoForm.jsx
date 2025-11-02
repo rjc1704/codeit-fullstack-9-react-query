450c65711d3cea7312d08d3364fcfa872f0b855a
Jecheon Ryu <rjc1704@gmail.com>
2025-11-02 17:49:35 +0900

    caching 실습준비

--- src/app/_components/TodoForm.jsx
+++ src/app/_components/TodoForm.jsx
@@ -1,30 +1,32 @@
 "use client";
 
+import { addTodo } from "@/lib/services/todos";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
-export default function TodoForm({ loadTodos }) {
+export default function TodoForm() {
   const [title, setTitle] = useState("");
 
-  const handleAddTodo = async (title) => {
-    try {
-      await addTodo(title);
-      // TODO: 할 일 추가 후 목록 다시 불러오기
-    } catch (err) {
-      console.error("할 일 추가 중 오류 발생:", err);
-      setError("할 일을 추가하는데 실패했습니다.");
-    }
-  };
+  const queryClient = useQueryClient();
+  const { mutate: mutateAddTodo, isPending: isAdding } = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error) => {
+      console.error("할 일 추가 중 오류 발생:", error);
+      alert("할 일 추가 중 오류가 발생했습니다.");
+    },
+    onSettled: () => {
+      setTitle("");
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!title.trim()) return;
 
-    try {
-      await handleAddTodo(title);
-      setTitle(""); // 입력 필드 초기화
-    } catch (err) {
-      console.error("할 일 추가 중 오류 발생:", err);
-    }
+    mutateAddTodo(title);
   };
 
   return (
@@ -38,7 +40,7 @@ export default function TodoForm({ loadTodos }) {
           className="flex-grow p-2 border"
         />
         <button type="submit" className="px-4 py-2 bg-blue-500 text-white">
-          추가
+          {isAdding ? "추가중..." : "추가"}
         </button>
       </div>
     </form>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,41 +1,14 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import TodoForm from "./_components/TodoForm";
 import TodoList from "./_components/TodoList";
-import { fetchTodos } from "@/lib/services/todos";
-import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const {
-    data: todos,
-    isPending,
-    error,
-  } = useQuery({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
-
-  if (isPending) {
-    return (
-      <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="container mx-auto px-4 py-8 text-center text-red-500">
-        {error.message}
-      </div>
-    );
-  }
-
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-8">투두리스트</h1>
       <div className="max-w-md mx-auto mt-8">
-        {/* <TodoForm loadTodos={loadTodos} /> */}
+        <TodoForm />
         <h2 className="text-2xl font-bold mb-4">할 일 목록</h2>
-        <TodoList todos={todos} />
+        <TodoList />
       </div>
     </div>
   );
